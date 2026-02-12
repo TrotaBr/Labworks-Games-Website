9bebc9073dd6ef9ea3bdf8f73a1b436aa877f58d
TrotaBr <daianamarcioli@gmail.com>
2026-02-12 15:27:48 -0300

    2° Try

--- public/src/Helpers/ModelHelper.js
+++ public/src/Helpers/ModelHelper.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
-const scenePath = '/public/models/scene.gltf'
+const scenePath = '/models/scene.gltf'
 
 export const LoadGLTFByPath = (scene, manager) => {
 
